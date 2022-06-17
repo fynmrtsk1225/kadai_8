6ea1f9fdce002609332a81c09a8fa4b26dbcadcb
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-06-17 15:49:38 +0900

    アカウント作成機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,23 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
   end
 
   def edit
     
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:image, :image_cache, :name, :email,
+                                  :password, :password_confirmation)
   end
   
 end
