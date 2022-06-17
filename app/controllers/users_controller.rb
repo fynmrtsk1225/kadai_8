84767a1774f118d76a546bcaac23b78ae227d2f6
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-06-17 13:33:05 +0900

    サインアップページを作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+  
+end
---
+++ db/migrate/20220617040506_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.text :image
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.index ["email"], name: "index_users_on_email", unique: true
+
+      t.timestamps
+    end
+  end
+end
