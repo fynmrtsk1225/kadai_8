242589c730332e745afe37b764da3d37936fd2c0
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-06-22 15:05:12 +0900

    サインアップと新規投稿でエラーメッセージを表示させる

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,10 @@
 class Picture < ApplicationRecord
   mount_uploader :image, CreateImageUploader
+
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
+  
+  validates :content, presence: true
+  validates :image, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :pictures
   has_many :favorites, dependent: :destroy
   has_many :favorite_pictures, through: :favorites, source: :picture
+  validates :image, presence: true
   validates :name, presence: true, length: { maximum: 30}
   validates :email, presence: true, length: { maximum: 255},
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
